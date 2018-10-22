b44d1f314d5fa7ea6a7ea364148c9f34d187ee26
koukous <hamane.el.mehdi@gmail.com>
2018-10-22 21:41:06 +0100

    first commit

---
+++ resources/views/contacte.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>contacte</h1>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>home</h1>
+@endsection
+
+@section('sidebar')
+@parent
+<p>this is added only to the home page</p>
+@endsection
